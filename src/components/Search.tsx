684ad9d6c5a9e3bc06f448cfecdc2d514d36f36f
salviogoncalvesoficial <salviodasilvacontato@gmail.com>
2026-09-08 23:16:25 +0200

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,34 +2,28 @@ import Fuse from "fuse.js";
 import { useEffect, useRef, useState, useMemo } from "react";
 import Card from "@components/Card";
 import type { CollectionEntry } from "astro:content";
-
 export type SearchItem = {
   title: string;
   description: string;
   data: CollectionEntry<"blog">["data"];
   slug: string;
 };
-
 interface Props {
   searchList: SearchItem[];
 }
-
 interface SearchResult {
   item: SearchItem;
   refIndex: number;
 }
-
 export default function SearchBar({ searchList }: Props) {
   const inputRef = useRef<HTMLInputElement>(null);
   const [inputVal, setInputVal] = useState("");
   const [searchResults, setSearchResults] = useState<SearchResult[] | null>(
     null
   );
-
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setInputVal(e.currentTarget.value);
   };
-
   const fuse = useMemo(
     () =>
       new Fuse(searchList, {
@@ -40,27 +34,23 @@ export default function SearchBar({ searchList }: Props) {
       }),
     [searchList]
   );
-
   useEffect(() => {
     // if URL has search query,
     // insert that search query in input field
     const searchUrl = new URLSearchParams(window.location.search);
     const searchStr = searchUrl.get("q");
     if (searchStr) setInputVal(searchStr);
-
     // put focus cursor at the end of the string
     setTimeout(function () {
       inputRef.current!.selectionStart = inputRef.current!.selectionEnd =
         searchStr?.length || 0;
     }, 50);
   }, []);
-
   useEffect(() => {
     // Add search result only if
     // input value is more than one character
     let inputResult = inputVal.length > 1 ? fuse.search(inputVal) : [];
     setSearchResults(inputResult);
-
     // Update search string in URL
     if (inputVal.length > 0) {
       const searchParams = new URLSearchParams(window.location.search);
@@ -72,7 +62,6 @@ export default function SearchBar({ searchList }: Props) {
       history.replaceState(history.state, "", window.location.pathname);
     }
   }, [inputVal]);
-
   return (
     <>
       <label className="relative block">
@@ -80,14 +69,14 @@ export default function SearchBar({ searchList }: Props) {
           <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
           </svg>
-          <span className="sr-only">Search</span>
+          <span className="sr-only">Buscar</span>
         </span>
         <input
           className="block w-full rounded border border-skin-fill 
         border-opacity-40 bg-skin-fill py-3 pl-10
         pr-3 placeholder:italic placeholder:text-opacity-75 
         focus:border-skin-accent focus:outline-none"
-          placeholder="Search for anything..."
+          placeholder="Busque por qualquer coisa..."
           type="text"
           name="search"
           value={inputVal}
@@ -97,17 +86,13 @@ export default function SearchBar({ searchList }: Props) {
           ref={inputRef}
         />
       </label>
-
       {inputVal.length > 1 && (
         <div className="mt-8">
-          Found {searchResults?.length}
-          {searchResults?.length && searchResults?.length === 1
-            ? " result"
-            : " results"}{" "}
-          for '{inputVal}'
+          {searchResults?.length === 1
+            ? `Encontrado 1 resultado para '${inputVal}'`
+            : `Encontrados ${searchResults?.length ?? 0} resultados para '${inputVal}'`}
         </div>
       )}
-
       <ul>
         {searchResults &&
           searchResults.map(({ item, refIndex }) => (

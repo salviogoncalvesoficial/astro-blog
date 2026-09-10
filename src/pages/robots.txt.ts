9e34a677673b4210d39d7a555fb14b31fb2f789b
salviogoncalvesoficial <salviodasilvacontato@gmail.com>
2026-09-10 08:07:11 +0200

    robots.txt: bloqueia /admin/ para todos os buscadores

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,9 +4,10 @@ import { SITE } from "@config";
 const robots = `
 User-agent: Googlebot
 Disallow: /nogooglebot/
+Disallow: /admin/
 
 User-agent: *
-Allow: /
+Disallow: /admin/
 
 Sitemap: ${new URL("sitemap-index.xml", SITE.website).href}
 `.trim();
